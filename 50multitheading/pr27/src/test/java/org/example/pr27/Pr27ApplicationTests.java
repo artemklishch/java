1f56ab5872239ed360ff1d4bdefce58628d0a8c6
Artem Klishch <artemklishch@gmail.com>
2024-10-28 11:24:26 +0200

    sdgsd

---
+++ 50multitheading/pr27/src/test/java/org/example/pr27/Pr27ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pr27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
